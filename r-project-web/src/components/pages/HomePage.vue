c0612ea58af48b0d5c399fa9585c3b8248bab31a
ladyishenlong <30828481+ladyishenlong@users.noreply.github.com>
2019-05-20 17:08:04 +0800

    vue 初次请求成功

---
+++ r-project-web/src/components/pages/HomePage.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="layout">
+    <layout>
+      <!--  头部导航栏    -->
+      <header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout_nav">
+            <menu-item name="1">
+              <icon type="ios-navigate">第一项</icon>
+            </menu-item>
+            <menu-item name="2">
+              <icon type="ios-navigate">第二项</icon>
+            </menu-item>
+            <menu-item name="3">
+              <icon type="ios-navigate">第三项</icon>
+            </menu-item>
+            <menu-item name="4">
+              <icon type="ios-navigate">第四项</icon>
+            </menu-item>
+          </div>
+
+        </Menu>
+      </header>
+
+      <!--   中间的内容   -->
+      <content>
+
+
+        <Button>Default</Button>
+        <Button type="primary" @click="hello">点击请求</Button>
+
+
+        <Button type="dashed">Dashed</Button>
+        <Button type="text">Text</Button>
+        <br><br>
+        <Button type="info">Info</Button>
+        <Button type="success">Success</Button>
+        <Button type="warning">Warning</Button>
+        <Button type="error">Error</Button>
+
+
+      </content>
+
+
+      <footer>
+        这里是底部
+      </footer>
+
+
+    </layout>
+  </div>
+</template>
+
+
+<script>
+  import axios from "axios";
+
+  export default {
+    name: 'HomePage',
+    data() {
+      return {
+        msg: 'this is  HomePage'
+      }
+    }, methods: {
+      test() {
+        alert("666")
+      },
+
+      hello: function () {
+        console.log("网络请求开始");
+        axios
+          .get("http://localhost:8003/test/test/hello")
+          .then(function (data) {
+            alert("请求成功：" + data)
+          })
+          .catch(function (error) {
+            console.log("请求异常：" + error)
+          })
+      }
+    }
+  }
+
+</script>
+
+
+<style scoped>
+  .layout_nav {
+    width: 420px;
+    margin: 0 20px 0 auto;
+  }
+</style>
--- r-project-web/src/main.js
+++ r-project-web/src/main.js
@@ -4,7 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+
+Vue.config.productionTip = false;
+Vue.use(iView);
+
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +17,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
