9afac2e8dd52597904cf06f2fa09c0fbcc52163f
ladyishenlong <30828481+ladyishenlong@users.noreply.github.com>
2019-05-28 08:59:18 +0800

    实现用户单账号登录

--- r-project-zuul/src/main/java/com/ladyishenlong/rprojectzuul/security/SecurityConfig.java
+++ r-project-zuul/src/main/java/com/ladyishenlong/rprojectzuul/security/SecurityConfig.java
@@ -1,18 +1,35 @@
 package com.ladyishenlong.rprojectzuul.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.session.ConcurrentSessionFilter;
 import org.springframework.web.cors.CorsUtils;
+
+import javax.annotation.Resource;
 
 /**
  * @Author ruanchenhao
  * @Date 2019-05-16 11:41
+ *
+ * 单一登录参考：https://blog.csdn.net/bandancer/article/details/84922015
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource
+    private SessionRegistry sessionRegistry;
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
 
     @Override
@@ -33,25 +50,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/user/login")
 
 
-                .successHandler(new SuccessHandler()).and()
-
+                //登录成功返回sessionId
+                .successHandler(new SuccessHandler())
+                .and()
 
                 //要是没有登录，抛出401异常
                 .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and()
-
-                .rememberMe()
+                .rememberMe().and()
 
                 //关闭csrf，如果不关闭可能导致post请求无效
                 //放跨域请求，如果不关闭需要添加csrf隐藏域
-                .and().csrf().disable()
+                .csrf().disable()
 
                 //第一个用户登录之后，同样的账号无法登录其他设备
-                //TODO 后登陆的顶掉前一个登陆的
                 .sessionManagement()
                 .maximumSessions(1)
-                .expiredUrl("/user/login")
+                //这句配置实现同样用户名登陆之后，之前的用户session过期
+                .sessionRegistry(sessionRegistry)
+
+//                .expiredUrl("/user/login")
 
         ;
     }
