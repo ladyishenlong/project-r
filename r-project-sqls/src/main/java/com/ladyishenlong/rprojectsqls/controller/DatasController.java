4d3dec9b585ff0806cd7865f6bfa640ad1df3c11
ladyishenlong <30828481+ladyishenlong@users.noreply.github.com>
2019-05-31 16:54:06 +0800

    jpa 多数据源配置

--- r-project-sqls/src/main/java/com/ladyishenlong/rprojectsqls/controller/DatasController.java
+++ r-project-sqls/src/main/java/com/ladyishenlong/rprojectsqls/controller/DatasController.java
@@ -1,7 +1,9 @@
 package com.ladyishenlong.rprojectsqls.controller;
 
 import com.ladyishenlong.rprojectsqls.jpa.jlcredit_jpa.ArticleIndexJpa;
+import com.ladyishenlong.rprojectsqls.jpa.log_jpa.LogJpa;
 import com.ladyishenlong.rprojectsqls.model.jlcredit_model.ArticleIndexModel;
+import com.ladyishenlong.rprojectsqls.model.log_model.LogModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,9 +22,17 @@ public class DatasController {
     @Autowired
     private ArticleIndexJpa articleIndexJpa;
 
+    @Autowired
+    private LogJpa logJpa;
+
     @GetMapping("/getArticleIndexList")
     public List<ArticleIndexModel> getArticleIndexList() {
         return articleIndexJpa.findAllByUsername("123");
+    }
+
+    @GetMapping("/getLog")
+    public List<LogModel> getLog() {
+        return logJpa.findAll();
     }
 
 }
--- r-project-sqls/src/main/java/com/ladyishenlong/rprojectsqls/jpa/log_jpa/LogJpa.java
+++ r-project-sqls/src/main/java/com/ladyishenlong/rprojectsqls/jpa/log_jpa/LogJpa.java
@@ -4,10 +4,14 @@ import com.ladyishenlong.rprojectsqls.model.log_model.LogModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @Author ruanchenhao
  * @Date 2019-05-31 16:28
  */
 @Repository
 public interface LogJpa extends JpaRepository<LogModel, Integer> {
+
+    List<LogModel> findAll();
 }
